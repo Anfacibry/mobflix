40415a76b2131e928d89ded0768a74e0311ebec5
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-11-07 16:32:17 -0300

    criando um pequeno banco de dados

---
+++ lib/data/dados.dart
@@ -0,0 +1,23 @@
+class Videos {
+  ///Lista de mapas com chave e valores que contem todos os dados dos videos
+  static const List<Map<String, String>> videos = [
+    {
+      "titulo": "OS 5 METODOS DA LISTA DO DART",
+      "imagem": "assets/images/conteudo001.png",
+      "visualizacao": "1,8",
+      "tempo": "7 meses",
+    },
+    {
+      "titulo": "DESMISTIFICANDO MOBILE EPO2",
+      "imagem": "assets/images/conteudo002.png",
+      "visualizacao": "1,8",
+      "tempo": "7 meses",
+    },
+    {
+      "titulo": "DESENVOLVIMENTO IOS PARA INICIANTES COM GIOVANNA MOELLER",
+      "imagem": "assets/images/conteudo003.png",
+      "visualizacao": "52",
+      "tempo": "9 meses",
+    }
+  ];
+}
